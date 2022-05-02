efd2f2836fcc6f6555306ab8b5f69626d21274de
TheBanana <nickmaster2222@gmail.com>
2022-05-01 21:52:34 -0400

    Update skid_buf.sv

--- skid_buf.sv
+++ skid_buf.sv
@@ -1,8 +1,10 @@
-module skid_buf #(parameter WIDTH)(//this is first word fall through
+//has no buffer register in pass-through operation, it just connects the input and output combinatorialy
+//as such this cannot fix timing issues, only syncronization issues, put registers in modules for timing
+module skid_buf #(parameter WIDTH)(
     input clk,
 	input srst,
 	input i_valid,	//whether the data coming from input is ready
-	output o_ready,	//whether the skid buffer is ready for data
+	output reg o_ready,	//whether the skid buffer is ready for data
 	input [WIDTH-1:0] in,
 	output o_valid,	//whether the data going to the output is valid
 	input i_ready,	//whether the output module is ready for data
@@ -16,24 +18,21 @@ module skid_buf #(parameter WIDTH)(//this is first word fall through
 			o_ready <= 0;
 			data_valid <= 0;
 		end else begin
-			if(i_valid && i_ready) begin//out will be equal to in
+			if(i_valid && i_ready) begin//out will be equal to in or the data in the buffer
 				data <= '0;
 				data_valid <= 0;
-				o_ready = 0;
+				o_ready = 1;
 			end else if(i_valid && !i_ready) begin//need to store into buffer
 				data <= in;
 				data_valid <= 1;
 				o_ready <= 0;
 			end else if(!i_valid && i_ready) begin
-				if(data_valid)
-					o_ready <= 1;
-				else begin
-					o_ready <= 0;
+				o_ready <= 1;
 				data <= '0;
 				data_valid <= 0;
 			end else begin
 				data <= data;
-				o_ready <= 0;
+				o_ready <= data_valid;
 				data_valid <= data_valid;
 			end
 		end
@@ -44,10 +43,7 @@ module skid_buf #(parameter WIDTH)(//this is first word fall through
 			out = data;
 			o_valid = 1;
 		end else if(i_valid && i_ready) begin
-			if(data_valid)
-				out = data;
-			else begin
-				out = in;
+			out = in;
 			o_valid = 1;
 		end else begin
 			out = 0;
